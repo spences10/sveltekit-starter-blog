5aff0d2345ce76c8071c13b94c0b31785b7d1cfe
spences10 <spences10apps@gmail.com>
2022-02-02 21:33:55 +0000

    refactor: :recycle: use adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import vercel from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 const config = {
@@ -10,7 +10,7 @@ const config = {
   ],
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
-    adapter: vercel(),
+    adapter: adapter(),
   },
 }
 
