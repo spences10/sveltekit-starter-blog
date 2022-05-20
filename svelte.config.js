95661024086805d8ca8420b85f3e991c292acf9f
spences10 <spences10apps@gmail.com>
2022-05-20 08:36:03 +0100

    chore: :arrow_down: install adapter vercell

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 
 const config = {
